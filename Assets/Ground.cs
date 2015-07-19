fb9f8fa00e2cf5e5e4390a1b351b57b9674b2c89
Furukazu Takahashi <yuki.ohiro@gmail.com>
2015-07-19 10:37:04 +0900

    init commit
    初期コミ〜

---
+++ Assets/Ground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		//if (coll.gameObject.tag == "Enemy")
+		//	coll.gameObject.SendMessage("ApplyDamage", 10);
+		print(coll);
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//Input.mousePosition
+		var t = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		t.z = this.transform.position.z;
+		this.transform.position = t;
+	}
+
+	//void OnCollisionEnter2D(Collision2D coll) {
+		//if (coll.gameObject.tag == "Enemy")
+		//	coll.gameObject.SendMessage("ApplyDamage", 10);
+		//print(coll);
+		//coll.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+		//coll.transform.parent = this.transform;
+
+	//}
+}
